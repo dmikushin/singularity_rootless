2f78957e89834b31350b4e03815229e9b1f74e03
Dmitry Mikushin <dmitry@kernelgen.org>
2024-04-03 20:07:20 +0200

    Reworking the startup script according to the typical service start/restart/stop/status interface

--- rockylinux8_service.sh
+++ rockylinux8_service.sh
@@ -12,51 +12,87 @@
 # to the server's main SSH connection via ProxyJump. This method allows to
 # login directly to the container via SSH, and connect remotely from VS Code.
 
+set -e
+
 CONTAINER_NAME=rockylinux8_rootless
 CONTAINER_BASE=/opt/$CONTAINER_NAME
 OVERLAY_USER=$HOME/rockylinux8_overlay
 
-# Create OverlayFS.
-mkdir -p $OVERLAY_USER/{lower,upper,work,merged}
+start() {
+    # Start Singularity instance, if not already started.
+    if singularity instance list | grep -q $CONTAINER_NAME; then
+        echo "$CONTAINER_NAME Singularity container is already started"
+    else
+        # Create a host key required by Dropbear, if not already existing
+        if [ ! -f $HOME/.ssh/${CONTAINER_NAME}_ed25519_host_key ]; then
+            $CONTAINER_BASE/usr/local/bin/dropbearkey -t ed25519 -f $HOME/.ssh/${CONTAINER_NAME}_ed25519_host_key
+        fi
+        if [ ! -f $HOME/.ssh/${CONTAINER_NAME}_ecdsa_host_key ]; then
+            $CONTAINER_BASE/usr/local/bin/dropbearkey -t ecdsa -f $HOME/.ssh/${CONTAINER_NAME}_ecdsa_host_key
+        fi
 
-# Mount OverlayFS, if not already mounted.
-# Note we set UID and GID so that all files belong to the current user,
-# which is not fully consistent, because some files may be required to belong
-# to root or nobody. However, this is a way to provide read-write access
-# within the overlay to the originally read-only base system.
-# In future we may wish to customize the fuse-overlayfs behavior to take
-# ownership of read-only file in case of legitimate write operation demand.
-if ! mountpoint -q "$OVERLAY_USER/merged"; then
-    fuse-overlayfs -o squash_to_uid=$(id -u) -o squash_to_gid=$(id -g) -o lowerdir=$CONTAINER_BASE,upperdir=$OVERLAY_USER/upper,workdir=$OVERLAY_USER/work $OVERLAY_USER/merged
-fi
+        # Create OverlayFS.
+        mkdir -p $OVERLAY_USER/{lower,upper,work,merged}
 
-# Create a host key required by Dropbear, if not already existing
-if [ ! -f $HOME/.ssh/dropbear_ed25519_host_key ]; then
-    $CONTAINER_BASE/usr/local/bin/dropbearkey -t ed25519 -f $HOME/.ssh/dropbear_ed25519_host_key
-fi
+        # Mount OverlayFS, if not already mounted. We don't use Singularity's --overlay
+        # option, because it does not do what we need.
+        # Note we set UID and GID so that all files belong to the current user,
+        # which is not fully consistent, because some files may be required to belong
+        # to root or nobody. However, this is a way to provide read-write access
+        # within the overlay to the originally read-only base system.
+        # In future we may wish to customize the fuse-overlayfs behavior to take
+        # ownership of read-only file in case of legitimate write operation demand.
+        if ! mountpoint -q "$OVERLAY_USER/merged"; then
+            fuse-overlayfs -o squash_to_uid=$(id -u) -o squash_to_gid=$(id -g) -o lowerdir=$CONTAINER_BASE,upperdir=$OVERLAY_USER/upper,workdir=$OVERLAY_USER/work $OVERLAY_USER/merged
+        fi
 
-# Start Singularity instance, if not already started.
-if ! singularity instance list | grep -q $CONTAINER_NAME; then
-    # Check which ports starting with 22221 are already occupied by existing
-    # processes, and use the next free port.
-    PORT=22221
-    while ss -tulwn | grep -q ":$PORT "; do
-        PORT=$((PORT + 1))
-    done
-    echo "Free port: $PORT"
+        # Check which ports starting with 22221 are already occupied by existing
+        # processes, and use the next free port.
+        PORT=22221
+        while ss -tulwn | grep -q ":$PORT "; do
+            PORT=$((PORT + 1))
+        done
 
-    while ss -tulwn | grep -q ":$PORT "; do
-        PORT=$((PORT + 1))
-    done
-    singularity instance start --writable --bind $HOME/.ssh/dropbear_ed25519_host_key:/etc/dropbear/dropbear_ed25519_host_key $OVERLAY_USER/merged $CONTAINER_NAME $PORT
-    echo "Now login to $CONTAINER_NAME container via SSH: ssh localhost -p $PORT -i <key_file>"
-else
-    echo "$CONTAINER_NAME Singularity container is already started"
-fi
+        singularity instance start --writable --bind $HOME/.ssh/${CONTAINER_NAME}_ed25519_host_key:/etc/dropbear/dropbear_ed25519_host_key --bind $HOME/.ssh/${CONTAINER_NAME}_ecdsa_host_key:/etc/dropbear/dropbear_ecdsa_host_key $OVERLAY_USER/merged $CONTAINER_NAME $PORT
+        echo "Now login to $CONTAINER_NAME container via SSH: ssh localhost -p $PORT -i <key_file>"
+    fi
+}
 
-# Stop Singularity instance, of not already stopped.
-if singularity instance list | grep -q $CONTAINER_NAME; then
-    singularity instance stop $CONTAINER_NAME
-else
-    echo "$CONTAINER_NAME Singularity container is not running"
-fi
+stop() {
+    # Stop Singularity instance, of not already stopped.
+    if singularity instance list | grep -q $CONTAINER_NAME; then
+        singularity instance stop $CONTAINER_NAME
+    else
+        echo "$CONTAINER_NAME Singularity container is not running"
+    fi
+    if mountpoint -q "$OVERLAY_USER/merged"; then
+        umount $OVERLAY_USER/merged
+    fi
+}
+
+status() {
+    if singularity instance list | grep -q $CONTAINER_NAME; then
+        echo "$CONTAINER_NAME Singularity container is running"
+    else
+        echo "$CONTAINER_NAME Singularity container is not running"
+    fi
+}
+
+case "$1" in
+start)
+    start
+    ;;
+stop)
+    stop
+    ;;
+restart)
+    stop
+    start
+    ;;
+status)
+    status
+    ;;
+*)
+    echo "Usage: $0 {start|stop|restart|status}"
+    ;;
+esac
